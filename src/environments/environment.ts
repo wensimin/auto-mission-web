11c2f2857960e9b1f347274b5f0a764d01bd6752
wensimin <hukaizhiyu2@gmail.com>
2022-03-31 21:39:54 +0800

    add clear button

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
   authSever: 'https://shali.fun:3000/authorization',
   // authSever: 'http://auth-server:81/authorization',
-  resourceServer :'http://127.0.0.1:8080/auto-mission',
-  // resourceServer: 'https://shali.fun:3000/auto-mission'
+  // resourceServer :'http://127.0.0.1:8080/auto-mission',
+  resourceServer: 'https://shali.fun:3000/auto-mission'
 };
 
 /*
