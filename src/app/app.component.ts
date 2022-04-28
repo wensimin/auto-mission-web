11e937dd3b63e03bca774cd79cb8bbc5712d9bbe
wensimin <hukaizhiyu2@gmail.com>
2022-04-28 17:37:51 +0800

    清除切换menu的焦点样式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import {Component} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {OAuthService} from "angular-oauth2-oidc";
 import {LoadingService} from "./service/loading.service";
 import {firstValueFrom} from "rxjs";
 import {ConfirmDialogComponent} from "./confirm-dialog/confirm-dialog.component";
 import {MatDialog} from "@angular/material/dialog";
+import {MatSidenav} from "@angular/material/sidenav";
+import {MatButton} from "@angular/material/button";
+import {FocusMonitor} from "@angular/cdk/a11y";
 
 @Component({
   selector: 'app-root',
@@ -15,11 +18,14 @@ export class AppComponent {
   isAuthed = false
   username = null
   loading = this.loadService.isLoading()
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+  @ViewChild('menu') menu!: MatButton;
 
   constructor(
     private oauthService: OAuthService,
     private dialog: MatDialog,
-    private loadService: LoadingService
+    private loadService: LoadingService,
+    private focusMonitor: FocusMonitor
   ) {
     this.loadAuth()
   }
@@ -40,5 +46,11 @@ export class AppComponent {
     }).afterClosed()).then()
     if (!confirm) return
     this.oauthService.revokeTokenAndLogout().then(() => this.isAuthed = this.oauthService.hasValidAccessToken())
+  }
+
+  closeNav() {
+    this.sidenav.close().then()
+    // 去除menu按钮的焦点样式
+    this.focusMonitor.stopMonitoring(this.menu._elementRef.nativeElement);
   }
 }
